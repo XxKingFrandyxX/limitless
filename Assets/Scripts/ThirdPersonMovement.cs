0ccb2abdb72e0bec0d853d8ad7161932909f3d49
XxKingFrandyxX <151718434+XxKingFrandyxX@users.noreply.github.com>
2024-07-29 11:39:31 +1000

    Prototype #1
    First

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -10,6 +10,11 @@ public class ThirdPersonMovement : MonoBehaviour
     Vector3 camForward;
     public float rotationSpeed = 180;
     Vector3 move;
+    CharacterController cc;
+    public float moveSpeed = 4;
+    public float jumpSpeed = 10.0f;
+    public float gravityMultiplier = 3;
+    float verticalVelocity = 0;
 
     void Inputs()
     {
@@ -19,9 +24,42 @@ public class ThirdPersonMovement : MonoBehaviour
 
     void Start()
     {
-        if (cam -- null)
+        cc = GetComponent<CharacterController>();
+
+        if (cam == null)
             {
                 cam = Camera.main.transform;
             }
+    }
+
+    void Rotation()
+    {
+        camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1).normalized);
+        move = forwardDirection * camForward + sideDirection * cam.right;
+
+        move.Normalize();
+        move = transform.InverseTransformDirection(move);
+
+        float turnAmout = Mathf.Atan2(move.x, move.z);
+        transform.Rotate(0, turnAmout * rotationSpeed * Time.deltaTime, 0);
+    }
+
+    void Movement()    
+    {
+        Vector3 direction = transform.forward * move.z * moveSpeed;
+
+        if (cc.isGrounded)
+        {
+            verticalVelocity = 0;
+        }
+        verticalVelocity += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && cc.isGrounded)
+        {
+            verticalVelocity = jumpSpeed;
+        }
+        direction.y = verticalVelocity;
+
+        cc.Move(direction * Time.deltaTime);
     }
 }
